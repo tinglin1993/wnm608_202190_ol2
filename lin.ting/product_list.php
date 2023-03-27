df52b874863ec9e447ccd4385bf2e76317751912
tinglin <ting.lin1993@gmail.com>
2023-03-26 23:12:45 -0400

    Add files via upload

---
+++ lin.ting/product_list.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+	
+	<div class="container">
+		<div class="card soft">
+			<h2>Product List</h2>
+
+			<ul>
+				<li><a href="product_item.php?id=1">product 1</a></li>
+				<li><a href="product_item.php?id=2">product 2</a></li>
+				<li><a href="product_item.php?id=3">product 3</a></li>
+				<li><a href="product_item.php?id=4">product 4</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
