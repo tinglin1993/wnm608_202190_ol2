4938aea19a9eec5fc3d8f9dd3a73a12668a4545f
tinglin <ting.lin1993@gmail.com>
2023-04-23 23:17:41 -0400

    Add files via upload

---
+++ lin.ting/product_confirmation.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FLUFFY</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/cart.css">
+    
+    <?php include "parts/web_meta.php"; ?>
+</head>
+<body>
+    <?php include "parts/navbar.php"; ?>
+    <header>
+        <div class="container">
+            <a href="index.html" class="logo">
+                <img src="img/logo.png">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="aboutus.php">About Us</a></li>
+                </ul>
+            </nav>
+	
+	<div class="container">
+		<div class="card soft">
+			<h2>Thank you for your purchase</h2>
+			<p><a href="shop.php">Continue Shopping</a></p>
+		</div>
+	</div>
+
+</body>
+</html>
