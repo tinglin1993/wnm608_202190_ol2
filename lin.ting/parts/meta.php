3564291b84188d704ec0ae3463cdeb9a15dcf1f4
tinglin <ting.lin1993@gmail.com>
2023-04-30 23:01:42 -0400

    Delete lin.ting directory

--- lin.ting/parts/meta.php
+++
@@ -1,8 +0,0 @@
-
-<meta name="viewport" content="width=device-width">
-
-<base href="https://itstinglin.com/public_html/tinglin/aau/wnm608/lin.ting/">
-
-<link rel="stylesheet" href="lib/css/styleguide.css">
-<link rel="stylesheet" href="lib/css/gridsystem.css">
-<link rel="stylesheet" href="css/storetheme.css">
